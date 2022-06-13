0852466615cf1b707c367dd87fac2ea0641c7618
ridwanrahadi <90244050+ridwanrahadi@users.noreply.github.com>
2022-06-13 14:59:13 +0700

    Delete inc.connection.php

--- inc.connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Variabel koneksi
-$serverName = "localhost";
-$connectionInfo = array( "Database"=>"", "UID"=>"", "PWD"=>"");
-$conn = sqlsrv_connect( $serverName, $connectionInfo );
-if( $conn === false ) {
-    die( print_r( sqlsrv_errors(), true));
-}
-?>
